7b74b783dd582e3c8e793f1a2ee2cb195063bd3d
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2025-06-12 17:24:54 +0300

    feat: add auth check

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,4 +1,20 @@
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 export default function Dashboard() {
+    const router = useRouter();
+    useEffect(() => {
+        // Check if the user is authenticated
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}auth/check`, {
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (!data.isAuthenticated) {
+                    router.push('/unauthorized');
+                }
+            });
+    }, [router]);
     return (
         <div className="min-h-screen flex items-center justify-center bg-[#F1F7F8]">
         <div className="w-[410px] bg-white rounded-lg shadow-lg border border-[#5093B4] p-10 flex flex-col gap-4">
